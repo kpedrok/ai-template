e23d841f20da19784d56d62bf02fc5d7a2930395
Pedro Boas <pedrokvb@gmail.com>
2024-07-19 23:51:40 -0300

    add components library

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main className=''>
       <div>Hello</div>
+      <Button>Click me</Button>
     </main>
   )
 }
